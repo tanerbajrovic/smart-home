99e7e8a5bb839a44ebb06d89ce983e7ef5f02c4c
Taner Bajrovic <tanerbajrovic@gmail.com>
2023-06-26 18:35:17 +0200

    Add class AlarmState

---
+++ alarm.py
@@ -0,0 +1,29 @@
+class AlarmState:
+    
+    # Constructor
+    def __init__(self, locked: bool):
+        self.code = None
+        self.locked = locked
+    
+    # Is the code set?
+    def hasCode(self) -> bool:
+        return self.code is not None
+    
+    # Is the alarm on?
+    def isLocked(self) -> bool:
+        return self.locked
+    
+    # Unlock
+    def unlock(self, code: str) -> None:
+        if self.code is None or self.code == code:
+            self.locked = True
+        
+    # Lock
+    def lock(self) -> None:
+        self.locked = True
+        
+    # Set code
+    def setCode(self, newCode: str) -> None:
+        self.code = newCode
+      
+
